cfe143f6615101ec68adee2ef52c585114db7f8c
fardinatabassum <fardinatabassum@gmail.com>
2023-01-11 15:54:54 -0500

    Added JavaScript, created variables, if-else statements and for loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,45 @@
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""); // "ABC" => ["A", "B", "C"]
+var lower = "abcdefghijklmnopqrstuvwxyz".split("");
+var numeric = "1234567890".split("");
+var symbols = "!@#$%^&*()".split("");
+
+var characterArray = [];
+var answer = "";
+
+function generatePassword() {
+  var passwordLength = prompt(
+    "How long do you want your password to be?\n8-128"
+  );
+  console.log("Password Length: " + passwordLength);
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Select within 8-128 characters");
+    generatePassword();
+  } else {
+    if (confirm("Would you like uppercase characters in your password?")) {
+      characterArray = characterArray.concat(upper);
+    }
+    if (confirm("Would you like lowercase characters in your password?")) {
+      characterArray = characterArray.concat(lower);
+    }
+    if (confirm("Would you like numerics?")) {
+      characterArray = characterArray.concat(numeric);
+    }
+    if (confirm("Would you like symbols?")) {
+      characterArray = characterArray.concat(symbols);
+    }
+    if (characterArray.length == 0) {
+      window.alert("Select at least 1 character type to create password");
+      generatePassword();
+    } else {
+      for (let i = 0; i < passwordLength; i++) {
+        let rando = Math.floor(Math.random() * characterArray.length);
+        answer += characterArray[rando];
+      }
+      return answer;
+    }
+  }
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,7 +49,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
